a6eb2df3079d6075dd8cc01d934a7819806c8824
Kasun Chamika De Mel <115131452+KasunChamikaDeMel@users.noreply.github.com>
2025-01-27 14:14:44 +0530

    Update nav_controller.dart

--- lib/controller/nav_controller.dart
+++ lib/controller/nav_controller.dart
@@ -1,10 +1,8 @@
 import 'package:get/get.dart';
 
 class NavController extends GetxController {
-  // Reactive index to track selected tab
   var selectedIndex = 0.obs;
 
-  // Method to update selected index
   void changeTab(int index) {
     selectedIndex.value = index;
   }
